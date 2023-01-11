3c0f320fa22794ecd39826a5b38a79f74daf879e
Ruby Morrison <morrisongreeneatis@yahoo.com>
2023-01-11 04:48:18 +0000

    first commit

---
+++ ruby.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Morrison Ruby</title>
+<body>
+<h1>Welcome to Morrison's World!</h1>
+<h2 style="color:yellow">
+<?php
+echo "Hostname: ".gethostname();
+?>
+</h2>
+<p>Thank you for coming!</p>
+</body>
+</html>
+
